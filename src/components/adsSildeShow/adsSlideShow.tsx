0aaaa96cbdcbd232ac7f382657cf9a6380ec8e00
masahiro-666 <masahirokenta666@gmail.com>
2024-12-01 14:00:09 +0700

    update: ads

--- src/components/adsSildeShow/adsSlideShow.tsx
+++ src/components/adsSildeShow/adsSlideShow.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './ads.css';
-import Ads1 from"../mockups/ads.png";
-import Ads2 from"../mockups/ads.png";
-import Ads3 from"../mockups/ads.png";
+import Ads1 from"../mockups/adsQ.png";
+import Ads2 from"../mockups/adsW.png";
+import Ads3 from"../mockups/adsE.png";
 
 const ads = [
     "https://shippingsea-images.s3.ap-southeast-1.amazonaws.com/ads/blackfriday.png",
--- src/components/movies.tsx
+++ src/components/movies.tsx
@@ -103,7 +103,7 @@ function Movies() {
     <>
       <div className="bg-white mx-20 page-container">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <h2 className="text-2xl font-bold tracking-tight text-gray-900">Movies</h2>
+          <h2 className="text-2xl text-center font-bold tracking-tight text-gray-900">Movies</h2>
 
           {/* Category Select Dropdown with click */}
           <div className="mt-4 flex justify-end" ref={dropdownRef}>
